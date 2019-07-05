fa355626a79a167d2d6d39cdac68f3b78e21168e
alreadydunn <51840478+alreadydunn@users.noreply.github.com>
2019-07-05 18:06:33 -0500

    Add files via upload

---
+++ scripts/myScripts.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var person = prompt("Please enter your name", "John Doe");
+   if (person != null) {
+    alert("Hello " + person + "! How are you today?");
+	console.log("Hello " + person + "! How are you today?");
+  }
+}
+
